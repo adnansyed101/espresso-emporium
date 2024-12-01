c85f1e7b8524a965ac84a4e185326dce3a813fee
adnan <sirgeant10@gmail.com>
2024-12-01 19:49:36 +0600

    Create coffee Model

---
+++ server/models/coffee.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const coffeeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  chef: {
+    type: String,
+    required: true,
+  },
+  supplier: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  category: mongoose.SchemaTypes.ObjectId,
+});
+
+const Coffee = mongoose.model("coffee", coffeeSchema);
+
+export default Coffee;
